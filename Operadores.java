85c5debded4cb91aeb726ef2f34426ca1537a80a
Vitor Rodrigues <vitor.bidi@gmail.com>
2023-02-14 14:13:06 +0000

    envio via mensagem

--- Operadores.java
+++ Operadores.java
@@ -1,4 +1,12 @@
 public class Operadores {
-    
+    public static void main(String[] vitao) {
+        
+        int idade;
+        idade = 16;
+        idade++;
+        System.out.println("Eu tenho " + idade + " anos.");
+        idade+=5;
+        System.out.println(idade);
+    }
     
 }
