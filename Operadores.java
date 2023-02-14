2e14f59e06babba3bddeb932ba93c6c14101a2c1
Vitor Rodrigues <vitor.bidi@gmail.com>
2023-02-14 14:42:46 +0000

    entrada de dados pelo teclado

--- Operadores.java
+++ Operadores.java
@@ -1,12 +1,24 @@
+import java.util.Scanner;
 public class Operadores {
     public static void main(String[] vitao) {
+
+        Scanner entrada = new Scanner(System.in);
+        int idade = 0;
+        int numero1;
+        int numero2;
+        int produto;
+        System.out.println("Digite sua idade: ");
+        idade = entrada.nextInt();
+        System.out.println("Você tem " + idade + " anos.");
+
+        System.out.println("Digite um número");
+        numero1=entrada.nextInt();
         
-        int idade;
-        idade = 16;
-        idade++;
-        System.out.println("Eu tenho " + idade + " anos.");
-        idade+=5;
-        System.out.println(idade);
+        System.out.println("Digite um número");
+        numero2=entrada.nextInt();
+        produto=numero1*numero2;
+
+        System.out.println("O produto desses dois números é: "+ produto +".");
     }
     
 }
