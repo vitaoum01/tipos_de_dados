f4edeb1c5bd4f3fcb1816aca69d3d0dcd4fce4d0
Vitor Rodrigues <vitor.bidi@gmail.com>
2023-02-17 11:40:00 +0000

    fatorial bla bla

--- EstruturaDeRepeticao.java
+++ EstruturaDeRepeticao.java
@@ -1,4 +1,36 @@
-public class EstruturaDeRepeticao {
-    
+public class EstruturaDeRepeticao { 
+    public static void main(String[] args) {
+        for (int i=0; i<=300; i++){
+            System.out.println(i);
+        }
 
+        System.out.println("");
+        int soma = 0;
+        for (int i=1; i<=1000;i++){
+        soma = soma + i;
+            System.out.println(soma);
+
+        }
+    
+        System.out.println("");
+        for (int i=0; i<=100;i+=3){
+            System.out.println(i);
+    }
+
+        System.out.println("");
+        int fatResult = 1;
+        for (int j=0;j<=10;j++){
+            for (int i=j;i>=1;i--){
+                if (i == 0){
+                    fatResult = 1;
+                    break;
+                } else{
+                    fatResult = j * fatResult;
+                }
+            }
+            
+            System.out.println("O fatorial de "+j +" = "+ fatResult);
+            fatResult = 1;
+        }
+}
 }
